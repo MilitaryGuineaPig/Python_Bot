6e0e945acefa39b0b348fb0bacf5cc75ec6efd8c
Vladyslav <vovapallag@gmail.com>
2023-03-17 15:32:21 +0100

    s

--- stats.py
+++ stats.py
@@ -4,7 +4,7 @@ import sys
 import datetime
 
 # link = sys.argv[1]
-link = "https://www.linkedin.com/in/vladpallah/"
+link = "https://github.com/MilitaryGuineaPig"
 response = requests.get(link)
 soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -17,7 +17,7 @@ url = f'https://api.github.com/users/{git_nickname}'
 response = requests.get(url)
 user_data = response.json()
 registration_date = user_data['created_at']
-registration_date = registration_date.text.strip() if registration_date else None
+registration = registration_date if registration_date else None
 
 
 education_elem = soup.find('div', class_='mb-3 js-user-profile-bio f4')
@@ -44,7 +44,7 @@ with open("user_info.txt", "w") as f:
     f.write(f"Git nickname: {git_nickname}\n")
     f.write(f"Education: {education}\n")
     f.write(f"Brief Information: \n")
-    f.write(f"GitHub was registered in {registration_date}, first contribution was pushed in august 2004. {full_name} has {followers_text} followers regarding to today’s date ({current_date}).\n")
+    f.write(f"GitHub was registered in {registration}, first contribution was pushed in august 2004. {full_name} has {followers_text} followers regarding to today’s date ({current_date}).\n")
     f.write(f"Current user is considered active. Detailed information is shown on graphs bellow:\n")
     f.write(f"\nGraph activity for last 3 month\n")
     f.write(f"\nCV: {cv_link}")
