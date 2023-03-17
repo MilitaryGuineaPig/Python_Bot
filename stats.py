028cb472bd460ba7e9539b6e9e436c3782658f42
Vladyslav <vovapallag@gmail.com>
2023-03-17 15:27:23 +0100

    l

--- stats.py
+++ stats.py
@@ -4,25 +4,26 @@ import sys
 import datetime
 
 # link = sys.argv[1]
-link = "https://www.linkedin.com/in/vladpallah/"
+link = "https://github.com/MilitaryGuineaPig"
 response = requests.get(link)
 soup = BeautifulSoup(response.content, 'html.parser')
 
+# work correctly
 full_name_elem = soup.find('span', class_='p-name vcard-fullname d-block overflow-hidden')
 full_name = full_name_elem.text.strip() if full_name_elem else None
 
 git_nickname = link.split("/")[-1]
 
+# does not work
 education_elem = soup.find('div', class_='mb-3 js-user-profile-bio f4')
 education = education_elem.text.strip() if education_elem else None
-
+# does not work
 followers_elem = soup.find('a', {'href': f'/{git_nickname}?tab=followers'})
 followers_text = followers_elem.find('span', {'class': 'text-bold'}).text.strip() if followers_elem else None
-
+# work
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-cv_elem = soup.find('a', href=lambda href: href and 'resume' in href)
-cv_link = cv_elem['href'] if cv_elem else None
+
 
 print('Full Name:', full_name)
 print('GitHub Nickname:', git_nickname)
@@ -32,11 +33,11 @@ print('Current Date:', current_date)
 print('CV Link:', cv_link)
 # Create a new text file and write the information to it
 with open("user_info.txt", "w") as f:
-    f.write(f"{full_name}'s dossier\n\n")
+    f.write(f"\t\t{full_name}'s dossier\n\n")
     f.write(f"Full Name: {full_name}\n")
     f.write(f"Git nickname: {git_nickname}\n")
     f.write(f"Education: {education}\n")
-    f.write(f"Brief Information\n")
+    f.write(f"\t\tBrief Information\n")
     f.write(f"GitHub was registered in september 1992, first contribution was pushed in august 2004. {full_name} has {followers_text} followers regarding to today’s date ({current_date}).\n")
     f.write(f"Current user is considered active. Detailed information is shown on graphs bellow:\n")
     f.write(f"\nGraph activity for last 3 month\n")
