c9ce8c109525a5c768ecc43c162f45e7e8c15d76
Kostiantyn Popravka <popravka1209@gmail.com>
2023-03-17 15:35:23 +0100

    updated date format

--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import datetime
+from dateutil import parser
 
 # link = sys.argv[1]
 link = "https://www.linkedin.com/in/vladpallah/"
@@ -16,7 +17,7 @@ git_nickname = link.split("/")[-1]
 url = f'https://api.github.com/users/{git_nickname}'
 response = requests.get(url)
 user_data = response.json()
-registration_date = user_data['created_at']
+registration_date =  parser.parse(user_data['created_at']).date()
 registration_date = registration_date.text.strip() if registration_date else None
 
 
